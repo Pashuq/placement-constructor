5e2952b72b36f53aaa7ad48c0bfb47cd5411655d
Pavel Smirnov <smrnvpvl@gmail.com>
2022-10-21 12:56:51 +0300

    feat: add staff on coords

--- src/components/PlacementGridElement/index.jsx
+++ src/components/PlacementGridElement/index.jsx
@@ -1,14 +1,56 @@
-import React from "react";
-import { WIDTH_AND_HEIGHT_BLOCK } from "../../const";
+import React, { useState } from "react";
+import { WIDTH_AND_HEIGHT_BLOCK, TARGET_PROP } from "../../const";
 
 function PlacementGridElement({ elementData }) {
+  const [elData, setElementData] = useState(elementData);
+
+  const parsedELData = isNotEmptyElement(elData) ? JSON.parse(elData) : null;
+
+  function isTargetElement(obj) {
+    return obj.hasOwnProperty(TARGET_PROP);
+  }
+
+  function isNotEmptyElement(elValue) {
+    if (elValue === "") {
+      return false;
+    }
+    return elValue;
+  }
+
   const itemStyles = {
     width: WIDTH_AND_HEIGHT_BLOCK + "px",
     height: WIDTH_AND_HEIGHT_BLOCK + "px",
     cursor: "Pointer",
   };
 
-  return <div style={itemStyles} className="placementfield__item"></div>;
+  const A = isNotEmptyElement(elData) ? (
+    isTargetElement(parsedELData) ? (
+      <div
+        style={itemStyles}
+        className="placementfield__item placementfield__item_disabled "
+      >
+        <img
+          alt={parsedELData.table}
+          src={parsedELData.imgUrl}
+          width={WIDTH_AND_HEIGHT_BLOCK * Number(parsedELData.sizeX)}
+          height={WIDTH_AND_HEIGHT_BLOCK * Number(parsedELData.sizeY)}
+        />
+      </div>
+    ) : (
+      <div
+        style={itemStyles}
+        className="placementfield__item placementfield__item_disabled "
+      >
+        d
+      </div>
+    )
+  ) : (
+    <div style={itemStyles} className="placementfield__item">
+      0
+    </div>
+  );
+
+  return A;
 }
 
 export default PlacementGridElement;
--- src/const/index.js
+++ src/const/index.js
@@ -1,1 +1,3 @@
 export const WIDTH_AND_HEIGHT_BLOCK = 12;
+
+export const TARGET_PROP = "table";
